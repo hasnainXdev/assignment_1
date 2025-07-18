d7166265d66fb7cd2ffc9651c8b75adc5033d9e3
Muhammad Hasnain <tryfrelancer23@gmail.com>
2025-07-18 23:56:58 +0500

    first commit assignment in progress

---
+++ product_suggestor.py
@@ -0,0 +1,26 @@
+from agents import Agent, OpenAIChatCompletionsModel, RunConfig, Runner, function_tool
+from openai import AsyncOpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client=external_client,
+)
+
+config = RunConfig(
+    model=model,
+    model_provider=external_client,
+    tracing_disabled=True,
+)
+
+
