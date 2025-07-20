e407d5d93cc5d80a32822f12636f75b0c7f18082
Muhammad Hasnain <tryfrelancer23@gmail.com>
2025-07-20 23:16:16 +0500

    updated

--- country_info_toolkit.py
+++ country_info_toolkit.py
@@ -1,13 +1,15 @@
-from agents import Agent, OpenAIChatCompletionsModel, RunConfig, Runner, function_tool
+from agents import Agent, OpenAIChatCompletionsModel, RunConfig, Runner, set_tracing_disabled
 from openai import AsyncOpenAI
 import asyncio
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
+set_tracing_disabled(disabled=True)
 
 # Set up Gemini Client
 gemini_api_key = os.getenv("GEMINI_API_KEY")
+
 external_client = AsyncOpenAI(
     api_key=gemini_api_key,
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
@@ -29,6 +31,7 @@ get_country_capital = Agent(
     instructions="""You are an expert at finding the capital city of a given country. When provided with a country name, 
         return only the name of the capital city as a single string (e.g., 'Islamabad' for Pakistan).
         Do not include additional text, explanations.""",
+        model=model
 )
 
 get_country_language = Agent(
@@ -38,6 +41,7 @@ get_country_language = Agent(
         return only the name of the primary national language as a single string (e.g., 'Urdu' for Pakistan). 
         Do not include additional text, explanations.
         """,
+        model=model
 )
 
 get_country_population = Agent(
@@ -46,43 +50,35 @@ get_country_population = Agent(
     You are an expert at retrieving the population of a given country. When provided with a country name,
         return only the population as a number (e.g., '220 million' for Pakistan). 
         Do not include additional text, explanations.""",
-)
-
-
-# Using `as_tool`
-
-get_country_capital_as_tool = get_country_capital.as_tool(
-    tool_name="get_country_capital",
-    tool_description="Retrieves the capital city of the given country as a single string.",
-)
-
-get_country_language_as_tool = get_country_language.as_tool(
-    tool_name="get_country_language",
-    tool_description="Retrieves the primary national language of the given country as a single string.",
-)
-
-
-get_country_population_as_tool = get_country_population.as_tool(
-    tool_name="get_country_population",
-    tool_description="Retrieves the population of the given country as a number.",
+        model=model
 )
 
 triage_agent = Agent(
-    name="Assistant",
-    instructions="""You are a smart Country Info Assistant. When given a country name
-        you will use the tools to get the capital, language, and population of that country.
+    name="Triage Agent",
+    instructions=(
+        """You are a smart Country Info Toolkit. When given a country name
+        important!
+        To get the capital of country, use the tool get_country_capital
+        To get the language of country, use the tool get_country_language
+        To get the population of country, use the tool get_country_population
         
-        You will use the tools provided to you to answer the question.
-        
-        To get the capital of country, use the tool `get_country_capital`
-        To get the language of country, use the tool `get_country_language`
-        To get the population of country, use the tool `get_country_population`
-        
-        """,
+        """
+    ),
+    model=model,
+
     tools=[
-        get_country_capital_as_tool,
-        get_country_language_as_tool,
-        get_country_population_as_tool,
+        get_country_capital.as_tool(
+            tool_name="get_country_capital",
+            tool_description="Retrieves the capital city of the given country as a single string.",
+        ),
+        get_country_language.as_tool(
+            tool_name="get_country_language",
+            tool_description="Retrieves the primary national language of the given country as a single string.",
+        ),
+        get_country_population.as_tool(
+            tool_name="get_country_population",
+            tool_description="Retrieves the population of the given country as a number.",
+        ),
     ],
 )
 
@@ -90,10 +86,8 @@ triage_agent = Agent(
 async def main():
     result = await Runner.run(
         triage_agent,
-        run_config=config,
-        input="what is the capital of pakistan including language and population?",
+        input="what is the capital of pakistan including population and language",
     )
-
     print(result.final_output)
 
 
